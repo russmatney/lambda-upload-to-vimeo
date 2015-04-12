c9359fa8f4cf9ae2afe4499271d38efe62f1e978
Russell Matney <russell.matney@gmail.com>
2015-04-12 16:42:42 -0400

    adds test file for quick debugging

---
+++ test.js
@@ -0,0 +1,13 @@
+console.log('running test file');
+
+var input = {
+  "sourceBucket": "russbosco",
+  "sourceKey": "events/awesomeparty201/timelapse/timelapse-final.mp4",
+  "musicCredit": "Music Credit",
+  "videoTitle": "Video Title"
+}
+
+var index = require('./')
+
+
+index.handler(input)
